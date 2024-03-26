85f43868c6ebbe512a3c3cd455ba6fec0d874315
blessing-leso <blessingitlotlengleso@gmail.com>
2024-03-26 21:59:34 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings: ["IT Graduate","Frontend Developer","Backend Developer" , "Web and App Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+})
